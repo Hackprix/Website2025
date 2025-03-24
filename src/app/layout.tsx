ba147d8f3d00debace4f9699fd076b34948d0d66
Mohd Mujahed <sfahad2904@gmail.com>
2025-03-24 14:49:42 +0530

    Add Vercel Analytics and implement Aurora background component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute={"class"} enableSystem defaultTheme="system">
         {children}
+        <Analytics />
         </ThemeProvider>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{ts,tsx,js,jsx}",
+];
+export const darkMode = "class";
+export const theme = {
+  extend: {
+    animation: {
+      aurora: "aurora 60s linear infinite",
+    },
+    keyframes: {
+      aurora: {
+        from: {
+          backgroundPosition: "50% 50%, 50% 50%",
+        },
+        to: {
+          backgroundPosition: "350% 50%, 350% 50%",
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+};
+export const plugins = [addVariablesForColors];
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
